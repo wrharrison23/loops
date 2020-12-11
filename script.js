f40a3c9a03cb14d4934e55fdde117fecde906d0e
Wes <wrharrison23@gmail.com>
2020-12-10 19:42:11 -0500

    Finished lightning

---
+++ script.js
@@ -0,0 +1,18 @@
+// Foundations - Loops
+
+// Lightning - Psychotic Bird
+let i = 1
+
+while (i < 101){
+    console.log('TWEET TWEET')
+    i++
+}
+
+// Lightning - Favorite Foods
+
+let favoriteFoods = ['chicken', 'mozz sticks', 'tuna', 'steak', 'fajitas']
+
+for(let x = 0; x < favoriteFoods.length; x++){
+    console.log(favoriteFoods[x])
+}
+
